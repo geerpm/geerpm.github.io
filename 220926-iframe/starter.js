195a86bc2e4b20fac8fde7a06fcc30ace3dcb20b
Funatos <geerpm+github@gmail.com>
2022-09-26 09:23:22 +0900

    Create starter.js

---
+++ 220926-iframe/starter.js
@@ -0,0 +1,18 @@
+
+
+
+(() => {
+  const target = document.getElementById("app");
+  const iframe = document.createElement("iframe");
+  iframe.width = 360;
+  iframe.height = 360;
+  iframe.frameBorder = "0";
+  iframe.scrolling = "no";
+  iframe.marginWidth = 0;
+  iframe.marginHeight = 0;
+
+  const src = "https://geerpm.github.io/220926-iframe/";
+  iframe.src = src;
+
+  target.appendChild(iframe);
+})();

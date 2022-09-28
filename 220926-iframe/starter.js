c37fb2a4baf028afb0ad53f2e864d05a4f6476d5
Funatos <geerpm+github@gmail.com>
2022-09-29 03:33:45 +0900

    Update starter.js

--- 220926-iframe/starter.js
+++ 220926-iframe/starter.js
@@ -1,22 +1,66 @@
-
-
+const BASE_URL = `https://geerpm.github.io/220926-iframe/`;
 
 (() => {
-  const target = document.getElementById("app");
-  const iframe = document.createElement("iframe");
-  iframe.width = 360;
-  iframe.height = 360;
-  iframe.frameBorder = "0";
-  iframe.scrolling = "no";
-  iframe.marginWidth = 0;
-  iframe.marginHeight = 0;
-  iframe.sandbox="allow-top-navigation allow-scripts allow-same-origin";
-
-  const src = "https://geerpm.github.io/220926-iframe/";
-  iframe.src = src;
-
-  target.appendChild(iframe);
-  
-  console.log(document.referrer);
-  console.log(`--------`);
+  // 即時
+  embedIfTargetExists();
 })();
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  // loaded
+  setTimeout(() => embedIfTargetExists(), 1000);
+});
+
+async function embedIfTargetExists() {
+  const searchTargetAttr = "data-sample";
+  const embedParamsAttrPrefix = "data-";
+  const promises = Array.from(
+    document.querySelectorAll(`[${searchTargetAttr}]`)
+  ).map(async (targetEl) => {
+    const targetEmbedType = targetEl.getAttribute(searchTargetAttr);
+    if (!targetEmbedType || targetEmbedType in EmbedTypes) {
+      return;
+    }
+    const data = Object.fromEntries(
+      Array.from(targetEl.attributes)
+        .filter((attr) => attr.nodeName.startsWith(embedParamsAttrPrefix))
+        .map((attr) => [
+          "x-" + attr.nodeName.substring(embedParamsAttrPrefix.length),
+          attr.value
+        ])
+    );
+    targetEl.setAttribute(searchTargetAttr, `done-${targetEmbedType}`);
+
+    // iframe構築
+    const iframe = document.createElement("iframe");
+    iframe.style.cssText = `
+    position: relative;
+    height: 100%;
+    width: 100%;
+    border: 0;
+    overflow: hidden;
+    `;
+    iframe.sandbox.add(
+      "allow-top-navigation",
+      "allow-scripts",
+      "allow-same-origin"
+    );
+
+    iframe.src = "about:blank";
+    targetEl.appendChild(iframe);
+
+    const src = `${BASE_URL}?${targetEmbedType}`;
+    try {
+      const res = await fetch(src, {
+        method: "GET",
+        headers: {
+          ...data
+        }
+      });
+      iframe.src = URL.createObjectURL(await res.blob());
+    } catch (e) {
+      // TODO error
+      return;
+    }
+  });
+  await Promise.all(promises);
+}

30fe575234a58e4ab1b9dee2c41850e19dbef107
Funatos <geerpm+github@gmail.com>
2022-09-29 13:00:13 +0900

    Update starter.js

--- 220926-iframe/starter.js
+++ 220926-iframe/starter.js
@@ -1,4 +1,6 @@
 const BASE_URL = `https://geerpm.github.io/220926-iframe/`;
+const searchTargetAttr = "data-sample";
+const embedParamsAttrPrefix = "data-";
 
 (() => {
   // 即時
@@ -11,11 +13,17 @@ window.addEventListener("DOMContentLoaded", (event) => {
   // loaded
   setTimeout(() => embedIfTargetExists(document), 1000);
 });
+window.addEventListener("message", (event) => {
+  // TODO
+  // if (e.origin !== "https://domain") {
+  //    return;
+  // }
+  doc.querySelectorAll(`[${searchTargetAttr}]`);
+  iframe.style.height = e.data + 40 + "px";
+});
 
 async function embedIfTargetExists(doc) {
   console.log(`-------    start 10s`);
-  const searchTargetAttr = "data-sample";
-  const embedParamsAttrPrefix = "data-";
   const promises = Array.from(
     doc.querySelectorAll(`[${searchTargetAttr}]`)
   ).map(async (targetEl) => {
@@ -87,25 +95,26 @@ function insertSizingCss(doc) {
   doc.head.appendChild(style);
   const styleSheet = style.sheet;
 
-  styleSheet.insertRule(`@media screen and (max-width: 576px) {
+  styleSheet.insertRule(`@media screen and ( max-width:520px) {
     [data-sample="done-index"] {
-      height: calc(100vw / 16 * 29);
+      height: 560px;
     }
     [data-sample="done-list"] {
       height: calc(100vw * 9.16);
     }
   }`);
-  styleSheet.insertRule(`@media screen and (min-width: 577px) and (max-width: 991px) {
+  styleSheet.insertRule(`@media screen and (min-width:521px) and ( max-width:849px) {
     [data-sample="done-index"] {
-      height: calc(100vw / 15 * 11);
+      height: 380px;
     }
     [data-sample="done-list"] {
       height: calc(100vw * 2.085);
     }
   }`);
-  styleSheet.insertRule(`@media screen and (min-width: 992px) {
+  styleSheet.insertRule(`@media screen and (min-width:850px) {
     [data-sample="done-index"] {
-      height: calc(100vw / 100 * 38);
+      height: 520px;
+      max-width: 1200px;
     }
     [data-sample="done-list"] {
       height: calc(100vw * 1.29);
